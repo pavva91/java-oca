3ea0a2cd790702105c95f697ee3b1b1ef339378e
pavva91 <valerio.mattioli@protonmail.com>
2022-03-29 10:58:09 +0200

    compile and run cli

---
+++ chapter_1/1_compile_and_run/compile_and_run.sh
@@ -0,0 +1,10 @@
+# COMPILE
+javac Zoo.java
+
+# RUN
+java Zoo
+
+# NOTE: Running the java command with .class extension will not work
+# java Zoo.class
+# Error: Could not find or load main class Zoo.class
+# Caused by: java.lang.ClassNotFoundException: Zoo.class
